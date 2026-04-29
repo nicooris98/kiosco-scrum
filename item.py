eaac399c764ba5b17fc65b8342b976fd003e20fb
Nicolas Oris <nicolasoris98@gmail.com>
2026-04-29 20:26:06 -0300

    agregado item repository

--- item.py
+++ item.py
@@ -1,9 +1,12 @@
 from product import Product
+from sell import Sell
 
 class Item:
-    def __init__(self, product: Product, quantity):
+    def __init__(self, product: Product, quantity, sell: Sell, id=None):
+        self._id = id
         self._product = product
         self._quantity = quantity
+        self._sell = sell
 
     @property
     def quantity(self):
@@ -12,3 +15,11 @@ class Item:
     @property
     def subtotal(self):
         return self._product.price * self._quantity
+    
+    @property
+    def product(self):
+        return self._product
+    
+    @property
+    def sell(self):
+        return self._sell
--- sell.py
+++ sell.py
@@ -4,8 +4,12 @@ from item import Item
 
 #Clase Venta
 class Sell:
-    def __init__(self):
-        self._date = dt.datetime.now().isoformat()
+    def __init__(self, date=None, id=None):
+        self._id = id
+        if(date):
+            self._date = date
+        else:
+            self._date = dt.datetime.now().isoformat()
         self._items = []
         self._total = 0
 
